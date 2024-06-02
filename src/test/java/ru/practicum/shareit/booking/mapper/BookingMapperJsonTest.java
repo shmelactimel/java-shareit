6aca8609e698f46c6a1656273b3f48ec609dd260
shmelactimel <2002erra@gmail.com>
2024-06-01 21:46:19 -0400

    Итерация 2.5

--- src/test/java/ru/practicum/shareit/booking/mapper/BookingMapperJsonTest.java
+++ src/test/java/ru/practicum/shareit/booking/mapper/BookingMapperJsonTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.booking.model.BookingShort;
 
 import java.time.LocalDateTime;
 
@@ -23,11 +22,9 @@ class BookingMapperJsonTest {
         booking.setId(1L);
         booking.setStart(LocalDateTime.now());
         booking.setEnd(LocalDateTime.now().plusDays(1));
-        // Добавьте другие поля booking, если необходимо
 
         String json = objectMapper.writeValueAsString(booking);
         assertThat(json).contains("\"id\":1");
-        // Дополнительные проверки JSON
     }
 
     @Test
@@ -38,14 +35,5 @@ class BookingMapperJsonTest {
         assertThat(booking.getId()).isEqualTo(1L);
         assertThat(booking.getStart()).isEqualTo(LocalDateTime.of(2023, 1, 1, 12, 0));
         assertThat(booking.getEnd()).isEqualTo(LocalDateTime.of(2023, 1, 2, 12, 0));
-    }
-
-    @Test
-    public void testDeserializeBookingShort() throws Exception {
-        String json = "{\"bookerId\":1,\"itemId\":2}";
-        BookingShort bookingShort = objectMapper.readValue(json, BookingShort.class);
-
-        assertThat(bookingShort.getBookerId()).isEqualTo(1L);
-        assertThat(bookingShort.getItemId()).isEqualTo(2L);
     }
 }
